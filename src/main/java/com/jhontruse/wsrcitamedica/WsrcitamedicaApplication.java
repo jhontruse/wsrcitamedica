756b2fcd42ea96ef43b634ba7e7bde7d7b798b16
Jhon Trujillo Serrano <jhontruse@gmail.com>
2025-12-01 12:41:12 -0500

    Creación del proyecto y configuración del POM

---
+++ src/main/java/com/jhontruse/wsrcitamedica/WsrcitamedicaApplication.java
@@ -0,0 +1,13 @@
+package com.jhontruse.wsrcitamedica;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WsrcitamedicaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WsrcitamedicaApplication.class, args);
+	}
+
+}
