0e14f7a7ffc2ba88797ee9fe1fe345c20d0b41e0
Jhon Trujillo Serrano <jhontruse@gmail.com>
2026-01-04 18:03:28 -0500

    Creación del proyecto wsrcitamedica

---
+++ src/test/java/com/jhontruse/wsrcitamedica/WsrcitamedicaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jhontruse.wsrcitamedica;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WsrcitamedicaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
